e018047e4327417e851d518665923648beac5329
Prateek <prateekgurnani10@gmail.com>
2019-03-15 18:37:31 -0500

    Development of project will be done here.

---
+++ pa8_C00124478_C00012875/pa8_C00124478_C00012875/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pa8_C00124478_C00012875
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
